03c089ebb17105ef34cc63562baeeb8f0a37a31a
nameejang <55773845+nameejang@users.noreply.github.com>
2020-07-06 10:50:12 +0900

    Initial commit

---
+++ assets/js/common.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $('.main-manu').click(function(){
+        $('#main-manu-link').toggleClass('hide');
+    })
+})
---
+++ index.php
@@ -0,0 +1,52 @@
+<html>
+<head>
+    <title>MeTube</title>
+    <link rel="stylesheet" href="./assets/css/style.css"/>
+    <link rel="stylesheet" href="./assets/css/reset.css"/>
+    <script type="text/javascript" src="./assets/js/jquery-1.11.3.min.js"></script>
+    <script type="text/javascript" src="./assets/js/common.js"></script>
+</head>
+<body>
+    <div id="container">
+        <div id="header">
+            <button class="main-manu">
+                <img src="./assets/images/menu.png" alt="">
+            </button>
+            <div class="logo">
+                <img src="./assets/images/mainlogo.png" alt="로고" width="100px">
+            </div>
+            <div class="search-wrap">
+                <form action="search.php">
+                    <input type="text" placeholder="search">
+                    <button>
+                        <img src="./assets/images/search.png" alt="검색" width="15px;">
+                    </button>
+                </form>
+            </div>
+        </div>
+        <div id="main-manu-link" class="hide">
+            <div class="header">
+                <button class="main-manu">
+                    <img src="./assets/images/menu.png" alt="">
+                </button>
+                <div class="logo">
+                    <img src="./assets/images/MainLogo_bk.png" alt="" width="100px">
+                </div>
+            </div>
+            <ul class="link-wrap">
+                <li><a href="index.php"><img src="./assets/images/icon_1.png" alt="">홈</a></li>
+                <li><a href="popular.php"><img src="./assets/images/icon_2.png" alt="">인기</a></li>
+            </ul>
+        </div>
+        <div id="nav-left">
+              <ul class="nav-button-wrap">
+                  <li><a href="index.php"><img src="./assets/images/icon_1.png" alt="">홈</a></li>
+                  <li><a href="popular.php"><img src="./assets/images/icon_2.png" alt="">인기</a></li>
+              </ul>
+        </div>
+        <div id="footer">
+        
+        </div>
+    
+</body>
+</html>
