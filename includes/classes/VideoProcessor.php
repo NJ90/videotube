8a8a409282c621fc87a8da0e47cf9724351ee0ae
nameejang <jangnamee@gmail.com>
2021-02-16 18:17:02 +0900

    31. Checking the file type of the uploaded video
    업로드 할 파일의 타입을 체크 - 올바르지 않은 타입의 파일을 올릴 때 메세지 출력

--- includes/classes/VideoProcessor.php
+++ includes/classes/VideoProcessor.php
@@ -2,6 +2,7 @@
 class VideoProcessor{
     private $con;
     private $sizeLimit = 500000000;
+    private $allowedTypes = array("mp4", "flv", "webm", "mkv", "vob", "ogv", "ogg", "avi", "wmv", "mov", "mpeg", "mpg");
 
     public function __construct($con){
         $this->con = $con;
@@ -12,26 +13,30 @@ class VideoProcessor{
         $targetDir = "uploads/videos/";
         $videoData = $videoUploadData->videoDataArray;
 
-        // mp4로 컨버트 되기 전 파일 패스 저장
-        // $tempFilePath = $targetDir . uniqid(); //아이디 출력 테스트
-        $tempFilePath = $targetDir . uniqid() . basename($videoData["name"]);
+            // mp4로 컨버트 되기 전 파일 패스 저장
+            // $tempFilePath = $targetDir . uniqid(); //아이디 출력 테스트
+            $tempFilePath = $targetDir . uniqid() . basename($videoData["name"]);
             // uploads/videos/uniqid filename
 
             //파일 패스 형태 변환
             $tempFilePath = str_replace(" ", "_", $tempFilePath);
 
-        $isValidData = $this->processData($videoData, $tempFilePath);
+            $isValidData = $this->processData($videoData, $tempFilePath);
 
-        echo $tempFilePath;
-
+            // echo $tempFilePath;
     }
 
     private function processData($videoData, $filePath) {
         $videoType = pathInfo($filePath, PATHINFO_EXTENSION);
 
-        // file-size 제한
-        if($this->isValidSize($videoData)){
+        // file-size 제한 메세지
+        if(!$this->isValidSize($videoData)){
             echo "File too large. Cant't be more than " . $this->sizeLimit . "bytes";
+            return false;
+        }
+        // file-type 제한 메세지
+        else if(!$this->isValidType($videoType)) {
+            echo "Invalid file type";
             return false;
         }
     }
@@ -39,5 +44,11 @@ class VideoProcessor{
     private function isValidSize($data){
         return $data["size"] <= $this->sizeLimit;
     }
+
+    //file-type 제한
+    private function isValidType($type){
+        $lowercased = strtolower($type);
+        return in_array($lowercased, $this->allowedTypes);
+    }  
 }
 ?>
--- processing.php
+++ processing.php
@@ -9,7 +9,7 @@ if(!isset($_POST["uploadButton"])) {
 }
 
 // 1) 업로드 데이터 생성
-$videoUpoadData = new VideoUploadData(
+$videoUploadData = new VideoUploadData(
     $_FILES["fileInput"], 
     $_POST["titleInput"],
     $_POST["descriptionInput"],
@@ -20,7 +20,7 @@ $videoUpoadData = new VideoUploadData(
 
 // 2) 비디오 데이터 업로드
 $videoProcessor = new VideoProcessor($con);
-$wasSuccessful = $videoProcessor->upload($videoUpoadData);
+$wasSuccessful = $videoProcessor->upload($videoUploadData);
 
 // 3) 업로드가 제대로 되었는지 확인
 
