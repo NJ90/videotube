4b6cf1da27773a8aff279d9c9f26fb5439cb19e4
nameejang <jangnamee@gmail.com>
2021-02-17 15:23:22 +0900

    31. Checking the file type of the uploaded video
    에러메세지 디버깅 : input name에  fileInput 정의해줌
    <input type='file' class='form-control-file' id='exampleFormControlFile1' name='fileInput' required>
    
    에러메세지 전문
    Notice: Undefined index: fileInput in /Applications/XAMPP/xamppfiles/htdocs/videotube/processing.php on line 13

--- includes/classes/VideoDetailsFormProvider.php
+++ includes/classes/VideoDetailsFormProvider.php
@@ -28,18 +28,13 @@
             </form>";
         }
 
-        private function createFileInput(){
-            return "<div class='form-group'>
-                        <label for='exampleFormControlFile' class='d-flex align-items-baseline'>
-                        <svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-plus' fill='currentColor' xmlns='http://www.w3.org/2000/svg'>
-                        <path fill-rule='evenodd' d='M8 3.5a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5H4a.5.5 0 0 1 0-1h3.5V4a.5.5 0 0 1 .5-.5z'/>
-                        <path fill-rule='evenodd' d='M7.5 8a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0V8z'/>
-                      </svg>
-                        File Input
-                        </label>
-                        <input type='file' class='form-control-file' id='exampleFormControlFile' required>
-                    </div>";
-        }
+        private function createFileInput() {
+
+          return "<div class='form-group'>
+                      <label for='exampleFormControlFile1'>Your file</label>
+                      <input type='file' class='form-control-file' id='exampleFormControlFile1' name='fileInput' required>
+                  </div>";
+      }
 
         private function createTitleInput(){
             return "<div class='form-group'>
